2bbaa503d645bb0a17cc4121557e226bce89b2d3
Андрей Боев <a.boev.it@gmail.com>
2024-05-04 19:58:46 +0300

    Fix. ДЗ

--- 5. Функции/Задачи/Рекурсия/Двоичный код(2).py
+++ 5. Функции/Задачи/Рекурсия/Двоичный код(2).py
@@ -1,8 +1,7 @@
 def to_bin(n):
     if n > 0:
-        binary = ''
-        binary += str(n % 2) + binary
-        return binary + to_bin(n//2)
-    else:
-        return ''
-print(to_bin(int(input()))[::-1])
+        return to_bin(n // 2) + str(n % 2)
+    return ''
+
+
+print(to_bin(int(input())))
--- 5. Функции/Задачи/Рекурсия/Двоичный код.py
+++ 5. Функции/Задачи/Рекурсия/Двоичный код.py
@@ -1,10 +1,12 @@
-n = int(input()) #136
+n = int(input())  # 136
+
 
 def to_bin(n):
     binary = ''
     while n > 0:
         binary = str(n % 2) + str(binary)  # 0
-        n //= 2 # 68
+        n //= 2  # 68
     return binary
+
 
 print(to_bin(n))
