a9bf907b5d131a10bbff68868d303726a1f081ad
Egor <e.pyzhikof2007@yandex.ru>
2024-07-24 12:05:30 +0300

    Решение задач по теме "Файлы"

---
+++ 10. Файлы/Задачи/Средний балл по классам.py
@@ -0,0 +1,19 @@
+scores = {9: [], 10: [], 11:[]}
+
+with open('input(1).txt', encoding='utf-8') as f:
+    lines = f.readlines()
+    for line in lines:
+        liness = line.strip().split()
+        first_name, last_name, rank, score = liness
+        rank, score = float(rank), float(score)
+
+        if rank in scores:
+            scores[rank].append(score)
+
+average_score = {}
+
+for rank, estimation in scores.items():
+    average_score[rank] = sum(estimation) / len(estimation)
+
+for a, b in average_score.items():
+    print(f'У {a} класса средняя оценка {b}')
